d8197ebfd719b40e3e5ff5e2a18cd91345384832
jgranstrom <jgranstrom@server.fake>
2013-03-28 11:14:20 +0100

    Added support for async parallel command execution

--- dev.js
+++ dev.js
@@ -2,24 +2,17 @@ Go = require('./lib/gonode.js').Go;
 
 // Dev stuff below
 go = new Go('devtest.go', true, function(err) {
-	go.on('data', function(json) {
-		console.log(json);
-
-		if(json.test === 'slut') {
-			go.close();
-			console.log('a');
-		}
-	});
 	go.on('error', function(err) {
-		console.log('error: ' + err);
+		console.log('error: ' + err.parser + ' ' + err.data);
 	})		
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'hej'});	
-	go.send({test: 'slut'});		
+
+	go.execute({test: 'a'}, response);	
+	go.execute({test: 'b'}, response);	
+	go.execute({test: 'c'}, response);	
+	go.execute({test: 'd'}, response);	
+	go.execute({test: 'e'}, response);	
 });
+
+function response(r) {
+	console.log(r);
+}
--- test/test-gonode.js
+++ test/test-gonode.js
@@ -10,14 +10,14 @@ exports.testInit = function(test) {
 		test.ok(err); // Make sure error is not null when there should be an error
 
 		// Does not fail init if file exists
-		go = new Go('./test/plaintest.go');
+		go = new Go('./test/echo.go');
 		go.init(function(err) {
 			test.ifError(err);
 
 			test.expect(3);
 			test.done();
 
-			go.close();
+			//go.close();
 		});
 	});	
 }
@@ -25,59 +25,13 @@ exports.testInit = function(test) {
 exports.testJSONEcho = function(test) {
 	var jsonData = {test: 'stuff', array: [1, 2, 3]};
 
-	go = new Go('./test/plaintest.go', true, function(err) {
-		go.on('data', function(json) {
-			test.equal(json.test, 'stuff');
-			test.equal(JSON.stringify(json.array), JSON.stringify([1, 2, 3]));
+	go = new Go('./test/echo.go', true, function(err) {
+		go.execute(jsonData, function(response) {
+			test.equal(response.test, 'stuff');
+			test.equal(JSON.stringify(response.array), JSON.stringify([1, 2, 3]));
 
 			test.expect(2);
 			test.done();
-
-			go.close();
 		});
-		
-		go.send(jsonData);
-	});
-}
-
-/* Test that a sequence of commands executed in order gets callback in sequential order 
- * This requirement may change when command execution has been further developed
- */
-
-exports.testSequentialCmds = function(test) {
-	var json1 = {test: 'stuff1', array: [1, 2, 3]};
-	var json2 = {test: 'stuff2', array: [4, 5, 6]};
-	var json3 = {test: 'stuff3', array: [7, 8, 9]};
-	var json4 = {test: 'stuff4', array: [10, 11, 12]};
-
-	var recievedIndex = 0;
-	go = new Go('./test/multitest.go', true, function(err) {
-		go.on('data', function(json) {
-			if(recievedIndex == 0) {
-				test.equal(json.test, 'stuff1');
-				test.equal(JSON.stringify(json.array), JSON.stringify([1, 2, 3]));
-			} else if(recievedIndex == 1) {
-				test.equal(json.test, 'stuff2');
-				test.equal(JSON.stringify(json.array), JSON.stringify([4, 5, 6]));
-			} else if(recievedIndex == 2) {
-				test.equal(json.test, 'stuff3');
-				test.equal(JSON.stringify(json.array), JSON.stringify([7, 8, 9]));
-			} else if(recievedIndex == 3) {
-				test.equal(json.test, 'stuff4');
-				test.equal(JSON.stringify(json.array), JSON.stringify([10, 11, 12]));
-
-				test.expect(8);
-				test.done();
-
-				go.close();
-			}
-
-			recievedIndex++;			
-		});
-		
-		go.send(json1);
-		go.send(json2);
-		go.send(json3);
-		go.send(json4);
 	});
 }
